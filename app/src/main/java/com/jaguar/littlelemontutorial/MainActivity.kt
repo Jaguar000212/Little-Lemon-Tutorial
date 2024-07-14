83a24df250ca8c473783e01d831bedfe01e64263
Shryansh Parashar <Jaguar000212@gmail.com>
2024-07-15 00:13:12 +0530

    Completed the first page with the cards as guided

--- app/src/main/java/com/jaguar/littlelemontutorial/MainActivity.kt
+++ app/src/main/java/com/jaguar/littlelemontutorial/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.jaguar.littlelemontutorial.ui.theme.LittleLemonTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LittleLemonTutorialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +27,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LittleLemonTutorialTheme {
-        Greeting("Android")
+fun HomeScreen(modifier: Modifier) {
+    Column(modifier = modifier) {
+        UpperPanel()
+        LowerPanel()
     }
 }
